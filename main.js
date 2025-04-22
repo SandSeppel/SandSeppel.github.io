54567f24d4b99f7cb2385ddf31930d0117c2bef8
Container <65561725+SandSeppel@users.noreply.github.com>
2025-04-22 19:35:57 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var cursor = document.querySelector('.cursor-gradient');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 66%), 0)`
+});
